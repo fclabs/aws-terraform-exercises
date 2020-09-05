7855e45dac2e58b81c6c503947229a64241c904a
fede <fede@mac>
2020-09-05 13:14:13 -0300

    Commit CloudFront Module

--- ha-wordpress/modules/cloudfront/main.tf
+++ ha-wordpress/modules/cloudfront/main.tf
@@ -3,22 +3,38 @@ resource "aws_cloudfront_distribution" "cfd" {
     default_cache_behavior {
         allowed_methods  = ["GET", "HEAD", "OPTIONS"]    
         cached_methods   = ["GET", "HEAD"]
-        viewer_cache_protocol = "allow-all"
+        viewer_protocol_policy = "allow-all"
         target_origin_id = var.target_origin_id
 
         forwarded_values {
             cookies {
                 forward = "none"
             } 
-            query_string = "*"           
+            query_string = false
         }
     }
 
     enabled = true
 
     origin {
-        s3_origin_config {}
+        origin_id = var.target_origin_id
+        s3_origin_config {
+            origin_access_identity = aws_cloudfront_origin_access_identity.cfoai.cloudfront_access_identity_path
+        }
 
         domain_name = var.origin_domain_name 
     }
+
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+}
+
+resource "aws_cloudfront_origin_access_identity" "cfoai" {
 }
