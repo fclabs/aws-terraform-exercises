6b94bcb42cede148406a1c3d8f08638b94ea7031
fede <fede@mac>
2020-09-15 09:52:27 -0300

    Added resource group to manage networking in HA-Wordpress

---
+++ ha-wordpress/networking/main.tf
@@ -0,0 +1,22 @@
+module "vpc" {
+    source = "../../modules/vpc"
+
+    vpc_name = var.lab_name
+    vpc_cidr  = var.vpc_cidr
+    networks = var.vpc_networks 
+}
+# output "vpc_subnet_privs" 
+# List of private subnet ids 
+# output "vpc_subnet_pubs" 
+# List of public subnets ids
+# output "id" 
+# VPC id
+
+# Create a Security Group for the Web Instance
+module sgweb_create {
+    source = "../../modules/security_group"
+
+    name = var.sg_name_websvr
+    vpc_id = module.vpc.id
+}
+
