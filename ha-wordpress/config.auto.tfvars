0f8288298c425e2d829c1e52be8ceda3f079eeb8
fede <fede@mac>
2020-09-15 18:25:04 -0300

    Reorganized lab code organization in resource groups

--- ha-wordpress/config.auto.tfvars
+++ ha-wordpress/config.auto.tfvars
@@ -1,7 +1,15 @@
+
+lab_name = "WP-HA-Stack"
+sg_name_websvr = "WP-HA-Stack-SG-WebServer"
+sg_name_lbfront = "WP-HA-Stack-SG-LB-Front"
+
 bucket_name_media = "fclabs-wordpress-ha-media"
 bucket_name_code = "fclabs-wordpress-ha-code"
+iam_instance_profile_name = "Wordpress-HA-S3-Buckets-Access"
+
 cf_target_origin_id = "S3Media-WP-HA"
 
 db_database = "wordpress"
 db_name = "wordpress-ha"
 
+elb_name = "Wordpress-HA-ELB-Front-Web"
--- ha-wordpress/config.tf
+++ ha-wordpress/config.tf
@@ -7,17 +7,25 @@ variable "aws_key_pair_pub" { type = string }
 variable "local_key_pair_priv" { type = string }
 variable "db_master_password" { type = string }
 variable "db_master_user" { type = string }
+variable "dns_zone" { type = string }
+
 ### Define Secret valies in secrets.auto.tfvars (not included in git repo)
 #########################################################A
 
+variable "lab_name" { type = string }
+variable "sg_name_websvr" { type = string }
+variable "sg_name_lbfront" { type = string }
+
 variable "bucket_name_media" { type = string }
 variable "bucket_name_code" { type = string }
+variable "iam_instance_profile_name" { type = string }
+
 variable "cf_target_origin_id" { type = string }
 
 variable "db_database" { type = string }
 variable "db_name" { type = string }
 
-
+variable "elb_name" { type = string }
 
 provider "aws" {
     access_key = var.aws_access_key 
@@ -25,11 +33,12 @@ provider "aws" {
     region     = "us-east-1"
 }
 
-## Network definitions
+## VPC Network definitions
 variable "vpc_cidr" { 
   default = "192.168.0.0/16" 
 }
 
+## VPC Subnets topology
 variable "vpc_networks" {
     default = [
       {
@@ -37,24 +46,31 @@ variable "vpc_networks" {
           az = "us-east-1a"
           public = true
           cidr_network = "192.168.1.0/24"
+          has_db = false
       },
       {
           name = "Public-east-1b"
           az = "us-east-1b"
           public = true
           cidr_network = "192.168.2.0/24"
+          has_db = false
       },
       {
           name = "Private-east-1a"
           az = "us-east-1a"
           public = false
           cidr_network = "192.168.3.0/24"
+          has_db = true
       },
       {
           name = "Private-east-1b"
           az = "us-east-1b"
           public = false
           cidr_network = "192.168.4.0/24"
+          has_db = true
       },
   ]
 }
+
+
+
