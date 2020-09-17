9456bda02218b9a1920dd8b6ca91ed353c649fc0
fede <fede@mac>
2020-09-17 16:44:59 -0300

    Fix  security groups rues

--- ha-wordpress/elb/main.tf
+++ ha-wordpress/elb/main.tf
@@ -15,26 +15,55 @@ locals {
     lb_subnets = [ for n in var.vpc_networks : n["cidr_network"] if n["public"] ]
 }
 
-data "aws_subnet" "db" {
+data "aws_subnet" "lb" {
     count = length( local.lb_subnets )
 
     vpc_id = data.aws_vpc.vpc.id
     cidr_block = local.lb_subnets[ count.index ]
 }
 
+# Add Web ingress rule for Web Server Instance
+module sglb_add_http {
+    source = "../../modules/security_group"
+
+    id = data.aws_security_group.sg_lb.id
+
+    operation = "add_cidr"
+
+    type = "ingress"
+    from = "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+}
+# Add Egreee rule for Web Server Instance
+module sglb_add_egress_all {
+    source = "../../modules/security_group"
+
+    id = data.aws_security_group.sg_lb.id
+
+    operation = "add_cidr"
+
+    type = "egress"
+    from = "0.0.0.0/0"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+}
+
 module "lb_web_front"  {
     
     source = "../../modules/elb"
     
-    lb_name = var.elb_name
+    lb_name = var.lb_name
     lb_create = true
 
     lb_security_groups = [ data.aws_security_group.sg_lb.id ]
-    lb_subnets = data.aws_subnet.db[*].id
+    lb_subnets = data.aws_subnet.lb[*].id
 
     lb_target_groups = [
         {
-            name = "WebFrontHTTP"
+            name = var.lb_target_group_name
             port = 80
             protocol = "HTTP"
             vpc_id = data.aws_vpc.vpc.id
@@ -45,16 +74,16 @@ module "lb_web_front"  {
 # ELB FQDN 
 
 # Register the ELB FQDN as www record on DNS zone 
-# module "dns_record" {
-#     source = "../../modules/route53"
+module "dns_record" {
+    source = "../../modules/route53"
 
-#     zone = var.dns_zone
-#     records = [
-#         {
-#             name = "www"
-#             ttl = 60
-#             type = "CNAME"
-#             values = [ module.elb_web_front.elb_fqdn ]           
-#         }
-#     ]
-# }
+    zone = var.dns_zone
+    records = [
+        {
+            name = "www"
+            ttl = 60
+            type = "CNAME"
+            values = [ module.lb_web_front.elb_fqdn ]           
+        }
+    ]
+}
