edab6c323e912e2d129c5abe7d6fb5e37949672f
fede <fede@mac>
2020-09-18 10:18:30 -0300

    Added new required outputs

--- modules/cloudfront/output.tf
+++ modules/cloudfront/output.tf
@@ -1,3 +1,11 @@
 output domain_name {
     value = aws_cloudfront_distribution.cfd.domain_name
 } 
+
+output arn {
+    value = aws_cloudfront_distribution.cfd.arn
+} 
+
+output oai_arn {
+    value = aws_cloudfront_origin_access_identity.cfoai.iam_arn
+}
