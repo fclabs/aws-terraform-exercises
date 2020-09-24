51b450ec42785fce29d1b7d027061a6ba35382f0
fede <fede@mac>
2020-09-24 15:50:41 -0300

    Aded bucker name and website endpoint as module output

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -1,7 +1,15 @@
+output "bucket" {
+    value = local.a_bucket.bucket
+}
+
 output "bucket_domain_name" {
     value = local.a_bucket.bucket_domain_name
 }
 
 output "arn" {
     value = local.a_bucket.arn
+}
+
+output "bucket_website_endpoint" {
+    value = local.a_bucket.website_endpoint
 }
