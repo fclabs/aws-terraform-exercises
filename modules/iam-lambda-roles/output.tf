11892b3ede7d16dbc7efc06650c2eeff27c5da16
fede <fede@mac>
2020-09-25 20:07:02 -0300

    Added arns list as output for module

--- modules/iam-lambda-roles/output.tf
+++ modules/iam-lambda-roles/output.tf
@@ -1,3 +1,7 @@
 output "lambda_roles" {
     value = [ for ra in aws_iam_role.instance_profile_role : ra.name ]
 }
+
+output "lambda_role_arns" {
+    value = { for ra in aws_iam_role.instance_profile_role : ra.name => ra.arn }
+}
